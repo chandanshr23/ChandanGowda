26da69d2141b61b7a1a75fc78fced3449f4c9623
Chandan Gowda <146355937+chandanshr23@users.noreply.github.com>
2025-05-21 02:03:23 +0530

    Update Problem4.java

--- src/com/tandemloop/test/Problem4.java
+++ src/com/tandemloop/test/Problem4.java
@@ -17,7 +17,7 @@ public class Problem4 {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter numbers separated by comma: ");
 		String s = scanner.nextLine();
-        String[] arr = s.trim().split(",");
+        String[] arr = s.trim().split(","); 	//give input -> Enter numbers seperated by comma:1,2,8,9,12,46,76,82,15,20,30
 
         List<Integer> input = new ArrayList<>();
         for (String inputList : arr) {
